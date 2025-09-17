3f109d08598d1b922bf5b23f7d4b6d0cfc8bb207
BMW <32.wardrop@gmail.com>
2025-09-16 22:23:56 -0600

    Okay Pepperoni

--- chapter4proj6.c
+++ chapter4proj6.c
@@ -17,7 +17,7 @@
 *   Perform computations necessary for check value
 *   Print check value to screen
 * End
-*******************************************************************************/
+******************************************************************************/
 
 #include <stdio.h>
 
@@ -29,8 +29,8 @@ int main(void)
     //prompt user for input
     printf("Enter the first 12 digits of the EAN number: ");
     //scan 12 single digit integer input values
-    scanf_s("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",&i1,&i2,&i3,&i4,&i5,&i6,&i7,&i8,&i9,&i10,
-        &i11,&i12);
+    scanf_s("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",&i1,&i2,&i3,&i4,&i5,&i6,&i7,
+        &i8,&i9,&i10,&i11,&i12);
     // perform computations necessary for check value
     sum1 = i2 + i4 + i6 + i8 + i10 + i12;
     sum2 = i1 + i3 + i5 + i7 + i9 + i11;
